4f0dc687c6eb40f23bad443a534c94e190ad6500
simon <simon@mirinfosys.com>
2025-04-21 17:08:12 +0600

    eslint config

--- app/page.js
+++ app/page.js
@@ -1,15 +1,4 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-import axios from "axios";
-import { headers } from "next/headers";
 import detectDevice from "../functions/deviceDetection";
-import { Button } from "@heroui/button";
 import HeroBanner from "@/components/PageWiseComponent/HomePage/HeroBanner";
 import SaleTimerBanner from "@/components/PageWiseComponent/HomePage/SaleTimerBanner";
 import NewsLetter from "@/components/PageWiseComponent/HomePage/NewsLetter";
